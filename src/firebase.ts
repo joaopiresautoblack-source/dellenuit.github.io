0b68257f277925212fe0e870936aabc9ceb6766f
joaopiresautoblack-source <joaopires.autoblack@gmail.com>
2026-07-19 00:35:08 -0300

    atl

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import firebaseConfig from "../firebase-applet-config.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAbIexqQYAKBy09CJvtVJ85BszzRTdboFQ",
+  authDomain: "bellenuit.firebaseapp.com",
+  projectId: "bellenuit",
+  storageBucket: "bellenuit.firebasestorage.app",
+  messagingSenderId: "399665430984",
+  appId: "1:399665430984:web:5620ed7753b7cb6d6880fd",
+  measurementId: "G-NZEN8H6DSJ"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export enum OperationType {
   CREATE = 'create',
